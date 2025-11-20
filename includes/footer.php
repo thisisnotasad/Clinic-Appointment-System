dead9809ed6e9dda32b46417db85d939a3cfdfda
Asad Ali Sajjad <imasad789@gmail.com>
2025-11-20 23:50:55 +0500

    Update register.php | Modify login.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 </div>
-    <footer class="bg-dark text-white text-center py-4 mb-0">
+    <footer class="bg-dark text-white text-center py-4 mt-5">
         <p>&copy; <?= date('Y') ?> MediCare Clinic System</p>
     </footer>
 </body>
